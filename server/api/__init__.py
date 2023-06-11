a0a6639648f2b7f000057f49b25e4a216fab8246
amenkes319 <amenkes319@gmail.com>
2023-06-11 18:31:44 -0400

    temp removed authentication

--- server/api/__init__.py
+++ server/api/__init__.py
@@ -1,13 +1,12 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from .app_instance import app
+import os
 
 db = SQLAlchemy()
 
 def create_app():
-    app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-
     db.init_app(app)
 
     from .routes import api_bp
