c550bd43cf642747d6adc2249b2e27cb9da1fd6a
amenkes319 <amenkes319@gmail.com>
2023-06-15 19:42:33 -0400

    Implemented item endpoints

--- server/api/routes/item_routes.py
+++ server/api/routes/item_routes.py
@@ -34,3 +34,69 @@ def create_item() -> Response:
     if not item:
         return make_response(jsonify({'error': 'Item already exists'}), 409)
     return make_response(jsonify(item.serialize()), 201)
+
+@bp.route('/<int:id>/', methods=['GET'])
+def get_item(id: int) -> Response:
+    """
+    Response to a GET request to /item/<item_id>. Gets an item from the database.
+
+    :param id: The ID of the item to get.
+
+    :return: Response with HTTP status of OK and the item.
+    Response with HTTP status of NOT_FOUND if the item does not exist.
+    """
+    item = item_service.get_item(id)
+    if not item:
+        return make_response(jsonify({'error': 'Item not found'}), 404)
+    return make_response(jsonify(item.serialize()), 200)
+
+@bp.route('/<int:id>/', methods=['PUT'])
+def update_item(id: int) -> Response:
+    """
+    Response to a PUT request to /item/<item_id>. Updates an item in the database.
+
+    :param id: The ID of the item to update.
+    :param item: An item object with the name, description, price, category, size, color, and stock of the item.
+
+    :return: Response with HTTP status of OK and the updated item.
+    Response with HTTP status of NOT_FOUND if the item does not exist.
+    """
+    data = json.loads(json.loads(request.form.get('data')))
+    
+    item = Item(**data)
+    item = item_service.update_item(id, item)
+    if not item:
+        return make_response(jsonify({'error': 'Item not found'}), 404)
+    return make_response(jsonify(item.serialize()), 200)
+
+@bp.route('/<int:id>/', methods=['DELETE'])
+def delete_item(id: int) -> Response:
+    """
+    Response to a DELETE request to /item/<item_id>. Deletes an item from the database.
+
+    :param id: The ID of the item to delete.
+
+    :return: Response with HTTP status of OK and the deleted item.
+    Response with HTTP status of NOT_FOUND if the item does not exist.
+    """
+    deleted = item_service.delete_item(id)
+    if not deleted:
+        return make_response(jsonify({'error': 'Item not found'}), 404)
+    return make_response(jsonify({'message': 'Item deleted'}), 200)
+
+@bp.route('/image/<int:id>/', methods=['PUT'])
+def update_item_image(id: int) -> Response:
+    """
+    Response to a PUT request to /item/image/<item_id>. Updates an item's image in the database.
+
+    :param id: The ID of the item to update.
+    :param image: An image file to upload.
+
+    :return: Response with HTTP status of OK and the updated item.
+    Response with HTTP status of NOT_FOUND if the item does not exist.
+    """
+    image = request.files.get('image')
+    item = item_service.update_item_image(id, image)
+    if not item:
+        return make_response(jsonify({'error': 'Item not found'}), 404)
+    return make_response(jsonify(item.serialize()), 200)
--- server/api/services/item_service.py
+++ server/api/services/item_service.py
@@ -4,7 +4,7 @@ from api.models.item import Item
 from api import db
 from werkzeug.datastructures import FileStorage
 
-def _upload_image(image: FileStorage) -> str:
+def _upload_image(image: FileStorage, name: str) -> str:
     """
     Uploads an image to the server.
 
@@ -12,15 +12,46 @@ def _upload_image(image: FileStorage) -> str:
 
     :return: The image URL.
     """
+    image.filename = name + os.path.splitext(image.filename)[1]
     save_directory = 'resources\\items'
     if not os.path.exists(save_directory):
         os.makedirs(save_directory)
 
-    filename = image.filename
     file_path = os.path.join(save_directory, image.filename)
     image.save(file_path)
 
     return file_path
+
+def _update_image_name(old_path: str, new_name: str) -> str:
+    """
+    Updates an image name.
+
+    :param old_path: The old image path.
+    :param new_name: The new image name.
+
+    :return: The updated image name.
+    """
+    if not old_path or not os.path.exists(old_path):
+        return None
+    
+    new_path = os.path.join(os.path.dirname(old_path), new_name + os.path.splitext(old_path)[1])
+    os.rename(old_path, new_path)
+
+    return new_path
+
+def _delete_image(image_url: str) -> bool:
+    """
+    Deletes an image from the server.
+
+    :param image_url: The image URL to delete.
+    
+    :return: True if the image was deleted, false if the image does not exist.
+    """
+    if not image_url or not os.path.exists(image_url):
+        return False
+    
+    os.remove(image_url)
+    return True
 
 def get_items() -> list[Item]:
     """
@@ -29,6 +60,16 @@ def get_items() -> list[Item]:
     :return: A list of items.
     """
     return Item.query.all()
+
+def get_item(id: int) -> Item:
+    """
+    Gets an item from the database.
+
+    :param id: The ID of the item to get.
+
+    :return: The item, none if the item does not exist.
+    """
+    return Item.query.filter_by(id=id).first()
 
 def create_item(item: Item, image: FileStorage) -> Item:
     """
@@ -42,8 +83,7 @@ def create_item(item: Item, image: FileStorage) -> Item:
     if item_exists:
         return None
     # file name is name with file extension
-    image.filename = item.name + os.path.splitext(image.filename)[1]
-    item.image_url = _upload_image(image) if image else None
+    item.image_url = _upload_image(image, item.name) if image else None
 
     max_id = db.session.query(func.max(Item.id)).scalar()
     new_id = int(max_id) + 1 if max_id else 1
@@ -53,3 +93,70 @@ def create_item(item: Item, image: FileStorage) -> Item:
     db.session.commit()
 
     return item
+
+def update_item(id: int, new_item: Item) -> Item:
+    """
+    Updates an item in the database.
+
+    :param id: The ID of the item to update.
+    :param new_item: An item object with the updated data.
+
+    :return: The updated item, none if the item does not exist.
+    """
+    item = Item.query.filter_by(id=id).first()
+    if not item:
+        return None
+    
+    item.name = new_item.name
+    item.description = new_item.description
+    item.price = new_item.price
+    item.category = new_item.category
+    item.size = new_item.size
+    item.color = new_item.color
+    item.stock = new_item.stock
+
+    new_path = _update_image_name(item.image_url, new_item.name)
+    if not new_path:
+        item.image_url = None
+
+    db.session.commit()
+
+    return item
+
+def update_item_image(id: int, image: FileStorage) -> Item:
+    """
+    Updates an item's image in the database.
+
+    :param id: The ID of the item to update.
+    :param image: An image file to upload.
+
+    :return: The updated item, none if the item does not exist.
+    """
+    item = Item.query.filter_by(id=id).first()
+    if not item:
+        return None
+    
+    deleted = _delete_image(item.image_url)
+    item.image_url = _upload_image(image, item.name)
+
+    db.session.commit()
+
+    return item
+
+def delete_item(id: int) -> bool:
+    """
+    Deletes an item from the database.
+
+    :param id: The ID of the item to delete.
+
+    :return: True if the item was deleted, false if the item does not exist.
+    """
+    item = Item.query.filter_by(id=id).first()
+    if not item:
+        return False
+    
+    deleted = _delete_image(item.image_url)
+    db.session.delete(item)
+    db.session.commit()
+
+    return True
